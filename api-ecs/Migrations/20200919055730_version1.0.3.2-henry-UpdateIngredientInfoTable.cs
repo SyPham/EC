3e08722af8f92b203b4c53cb1765dd1b5118df7c
henry-pham <phamtiensy8696@gmail.com>
2020-09-19 13:00:49 +0700

    fix confix merge by henry

---
+++ api-ecs/Migrations/20200919055730_version1.0.3.2-henry-UpdateIngredientInfoTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EC_API.Migrations
+{
+    public partial class version1032henryUpdateIngredientInfoTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiredDate",
+                table: "Ingredients");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiredDate",
+                table: "Ingredients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
