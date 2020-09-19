dfd40b72553fe01e142b06dffe577a30e789c483
henry-pham <phamtiensy8696@gmail.com>
2020-09-19 09:19:18 +0700

    edit migration by henry

---
+++ api-ecs/Migrations/20200919020904_version1.0.3-henry-UpdateIngredientTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EC_API.Migrations
+{
+    public partial class version103henryUpdateIngredientTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiredDate",
+                table: "Ingredients",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiredDate",
+                table: "Ingredients");
+        }
+    }
+}
